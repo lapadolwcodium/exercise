7d748bc907a6d51e05925bebe1d2b391ca1e41cf
ton <Lapadol.w@codium.co>
2017-10-25 18:49:22 +0700

    snake case and ignore extension file iml and xml

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,41 +1,37 @@
 def is_isogram(string):
-    isPass = False
+    is_pass = False
 
-    if(not string):
-        isPass = True
-    
-    if(string.islower()):
-        isPass = True
-        
-    if("-" in "thumbscrew-japingly"):
-        isPass = True
-        
-    centerString = string[1:-1]
-    for cn in centerString :
-        if(centerString.count(cn) > 1):
-            isPass = False
+    if not string:
+        is_pass = True
+
+    if string.islower():
+        is_pass = True
+
+    if "-" in string:
+        is_pass = True
+
+    center_string = string[1:-1]
+    for cn in center_string:
+        if center_string.count(cn) > 1:
+            is_pass = False
             break
 
-
-    duplicateNone = False
-    duplicateNor = False
+    duplicate_none = False
+    duplicate_nor = False
     newString = string.lower()
     validNonLetters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    
-    for cc in newString :
-        if(cc not in validNonLetters):#
-            if (newString.count(cc) > 1):
-                isPass = True
-                duplicateNone = True
+
+    for cc in newString:
+        if cc not in validNonLetters:  #
+            if newString.count(cc) > 1:
+                is_pass = True
+                duplicate_none = True
         else:
-            if (newString.count(cc) > 1):
-                isPass = False
-                duplicateNor = True
+            if newString.count(cc) > 1:
+                is_pass = False
+                duplicate_nor = True
 
-    if(duplicateNor and duplicateNone):
-        isPass = False
+    if duplicate_nor and duplicate_none:
+        is_pass = False
 
-
-    
-
-    return isPass
+    return is_pass
--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,19 +1,19 @@
 def is_leap_year(year):
 
-    isLeap = False
+    is_leap = False
 
-    if (year % 4 == 0) :
-        isLeap = True
+    if year % 4 == 0:
+        is_leap = True
 
-    if (year % 400 == 0) :
-        isLeap =  True
+    if year % 400 == 0:
+        is_leap =  True
 
-    if(year % 4 == 0 and year % 100 != 0) :
-        isLeap =  True
+    if year % 4 == 0 and year % 100 != 0:
+        is_leap =  True
         
-    if(year % 100 == 0 and year % 400 != 0) :
-        isLeap = False
+    if year % 100 == 0 and year % 400 != 0:
+        is_leap = False
     
-    return isLeap
+    return is_leap
 
     
